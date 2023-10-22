0c34c3dce75311cd4b015e3ab22a53ed1fd8f6d3
Macodou Yandé <vincenzatto@gmail.com>
2023-10-22 16:39:08 +0200

    ERRATUM : fix jms server config after maven project reload

--- sfg-jms/src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ sfg-jms/src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,8 +1,5 @@
 package guru.springframework.sfgjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
