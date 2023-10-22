a9f6d8e96add6f03f9a4fa23284a0c3581596d78
Macodou Yandé <vincenzatto@gmail.com>
2023-10-22 15:58:39 +0200

    nuances of Message types (Spring generic vs Apache ActiveMQ vs javax.jms)

--- sfg-jms/src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ sfg-jms/src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -21,24 +21,31 @@ public class HelloMessageListener {
 
     private final JmsTemplate jmsTemplate;
 
+    /**
+     * When a JMS message isn't properly received, the transaction is considered incomplete => JMS will send it again
+     * Can test this feature in debug mode while throwing exception and looking into headers variable
+     */
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message) {
-        System.out.println("Received a new message : \"" + helloWorldMessage + "\"");
 
-        /**
-         * When a JMS message isn't properly received, the transaction is considered incomplete => JMS will send it again
-         * Can test this feature in debug mode while throwing exception and looking into headers variable
-         */
+        System.out.println("Received a new message : \"" + helloWorldMessage + "\"");
         //throw new RuntimeException("foo");
     }
 
+    /**
+     * In debug mode, peek on springMessage variable to see its content
+     * Spring message abstraction is preferable for maintenance when evolving from JMS to other message API
+     * see {@link org.springframework.messaging.support.GenericMessage}
+     */
     @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
-    public void listenAndGreetLuffy(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message) throws JMSException {
+    public void listenAndGreetLuffy(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message, org.springframework.messaging.Message<HelloWorldMessage> springMsg) throws JMSException {
         HelloWorldMessage payloadMsg = HelloWorldMessage.builder()
                 .id(UUID.randomUUID())
                 .message(helloWorldMessage.getMessage() + " I will become the Pirate King!")
                 .build();
 
-        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
+        //jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
+
+        jmsTemplate.convertAndSend((Destination) springMsg.getHeaders().get("jms_replyTo"), payloadMsg);
     }
 }
